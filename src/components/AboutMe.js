e720e370d1c9e29dc9cddc2a3085e571a22df6bb
Daniel Valentini <daniel.valentini@libertymutual.com>
2018-07-02 14:05:02 -0400

    Added about me section

---
+++ src/components/AboutMe.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import osu from '../images/block_o.png';
+import { Wrapper, Title, Image, Info } from './styles/AboutMeComponents';
+
+class Education extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Title>
+          Education
+        </Title>
+        <Image src={osu} />
+        <Info>
+          B.S. Computer Science & Engineering<br/>
+          Class of 2016<br/>
+          Alpha Epsilon Pi
+        </Info>
+      </Wrapper>
+    );
+  }
+}
+
+export default Education;
